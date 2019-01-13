12353a204a0a3c711cdfe587fddc685df0d1799d
Victor <victorzuanazzi@gmail.com>
2019-01-13 01:12:43 +0100

    3.17- Plotting multiple stocks

---
+++ plotting_multiple_stocks.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 01:03:41 2019
+Coursera: https://classroom.udacity.com/courses/ud501/lessons/3975568860/concepts/41007385970923#
+Lesson 3, item 17 - Plorring multiple stocks
+
+@author: Victor Zuanazzi
+"""
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+def symbol_to_path(symbol, base_dir=""):
+    """"Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, f"{str(symbol)}.csv")
+
+def get_df(data_frame, symbol, columns, jhow = "left"):
+    path = symbol_to_path(symbol)
+    df_temp = pd.read_csv(path,
+                          index_col="Date",
+                          parse_dates = True,
+                          usecols = columns,
+                          na_values= ["nan"])
+    df_temp = df_temp.rename(columns={columns[1] : symbol})
+    data_frame = data_frame.join(df_temp, how = jhow)
+    return data_frame
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbos from CSV files."""
+    data_frame = pd.DataFrame(index = dates)
+    
+    #"SPY" will always be added in the beggining as refference
+    if "SPY" in symbols:
+        #If SPY is already present, we delet it first to avoid duplicates.
+        symbols.pop(symbols.index("SPY"))
+    data_frame = get_df(data_frame, "SPY", ["Date", "Adj Close"], jhow = "inner")
+    
+    #Add all symbols of the list to the dataframe
+    for s in symbols:
+        data_frame = get_df(data_frame, s, ["Date", "Adj Close"])
+            
+    return data_frame
+
+def plot_data(df, t = "Stock prices"):
+    """"Plot stock prices"""
+    ax = df.plot(title = t, fontsize=2)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
+
+def test_run():
+    """"Function called by test run"""
+    
+    #Define date range:
+    start_date = "2010-01-01"
+    end_date = "2014-12-31"
+    dates = pd.date_range(start_date, end_date)
+    
+    #ymbols = ["HCP", "STZ", "BBBY", "CHK", "AAPL", "IBM", "WMT", "PG", "XOM"]
+    symbols = ["HCP", "STZ", "BBBY"]
+    symbols.sort()
+    
+    df1 = get_data(symbols, dates)
+    plot_data(df1)
+    
+    
+if __name__ == "__main__":
+    test_run()
