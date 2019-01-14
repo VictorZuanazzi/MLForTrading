ae2bffcb04b44679e260fe7f2f4a246b2f17172c
Victor <victorzuanazzi@gmail.com>
2019-01-14 18:24:46 +0100

    5.8 rolling statistics

--- computing_rolling_statistics.py
+++ computing_rolling_statistics.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 14 17:27:12 2019
+Created on Mon Jan 14 17:42:53 2019
 Coursera: https://classroom.udacity.com/courses/ud501/lessons/4156938722/concepts/41890188580923
-Lesson 5, item 4 Compute global Statistics
+Lesson 5, item 8 Compute rolling statistics
 
 @author: Victor Zuanazzi
 """
@@ -18,23 +18,26 @@ def test_run():
     """"Function called by test run"""
     
     #Define date range:
-    start_date = "2010-01-01"
+    start_date = "2014-01-01"
     end_date = "2014-12-31"
     
     symbols = ["HCP", "STZ", "BBBY", "CHK", "AAPL", "IBM", "WMT", "PG", "XOM"]
     symbols.sort()
     
     w = ml.df_4_trading(symbols, start_date, end_date)
-    #w.plot_normalized("SPY", "2010-01-01", "2011-01-01")
-    print("Mean:")
-    print(w.df.mean())
-    print("Median:")
-    print(w.df.median())
-    print("std:")
-    print(w.df.std())
+    #w.plot_stock_prices(w.df["SPY"])
+    df = w.df
     
+    #compute rolling mean using a 20-day window   
+    rolling_mean = w.df["SPY"].rolling(20).mean()
     
-
+    #compute rulling std using a 20-day window
+    rustd = rolling_mean + w.df["SPY"].rolling(20).std()
+    rlstd = rolling_mean - w.df["SPY"].rolling(20).std()
+    
+    #plot rolling mean:
+    w.compare_plots(w.df["SPY"], start_date, end_date, rolling_mean)
+    
     
 if __name__ == "__main__":
     test_run()
